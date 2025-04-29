d7c2c49027dad711d54559937980c224d76995b7
Carlos06DEV <carloseduardobp.06@gmail.com>
2025-04-28 22:02:59 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,41 @@
-function saveFicha() {
-    /*let nome = document.getElementById('nome').value;
-    let email = document.getElementById('email').value;*/
-    let btn = document.getElementById('cta');
+const openBtns = document.querySelectorAll('.cta');
+const closeBtn = document.getElementById('closeModalBtn');
+const overlay = document.getElementById('overlay');
 
-    /*if (!email||!nome) {
-        alert("O email e nome são obrigatórios!");
+openBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+      overlay.style.display = 'flex';
+    });
+  });
+
+closeBtn.addEventListener('click', () => {
+  overlay.style.display = 'none';
+});
+
+overlay.addEventListener('click', (e) => {
+  if (e.target === overlay) {
+    overlay.style.display = 'none';
+  }
+});
+
+
+function saveFicha() {
+    let nome = document.getElementById('nome').value;
+    let email = document.getElementById('email').value;
+    let cargo = document.getElementById('cargo').value
+    let btn = document.getElementById('save');
+
+    if (!cargo) {
+        cargo = 'null'
+    }
+
+    if (!email||!nome) {
+        alert("O email e o nome são obrigatórios!");
         return;
     }
+
+    btn.disabled = true;
+    btn.innerHTML = "Carregando...";
 
     fetch('http://127.0.0.1:5000/save', {
         method: 'POST',
@@ -16,11 +45,10 @@ function saveFicha() {
         body: JSON.stringify({
             nome: nome,
             email: email,
+            cargo: cargo,
             }),
             mode: 'cors'
         })
-    */
-    btn.disabled = true;
-    btn.innerHTML = "Carregando...";
+
     window.location.href = "https://www.youtube.com/";
 }
